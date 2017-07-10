bbd01f65fa4f45763387f0c5ebeb742ab3723355
Ubuntu <warren.kushner@gmail.com>
2017-07-10 01:59:15 +0000

    added logic to load data from s3

--- data_aws/upload_data.py
+++ data_aws/upload_data.py
@@ -15,6 +15,7 @@ bucket = bucket = s3.Bucket(bucket_name)
 # for key in bucket.objects.get('fold4'):
 #     print(dir(key))
 import os
+import shutil
 dest = 'data/'
 def upload(directories):
     """
@@ -24,8 +25,7 @@ def upload(directories):
         directory_path= dest+directory
         if not os.path.exists(directory_path):
             os.makedirs(directory_path)
-        for key in bucket.objects.filter(Prefix=directory):
-            
+        for key in bucket.objects.filter(Prefix=directory+'/'):
             sound_file = dest+key.key
             try:
                 fh = open(sound_file,'r')
@@ -33,3 +33,8 @@ def upload(directories):
             # if file does not exist, create it
                 fh = open(sound_file,'w')
             bucket.download_file(key.key, sound_file)
+
+def delete_dirs(directories):
+    for directory in directories:
+        directory_path= dest+directory
+        shutil.rmtree(directory_path)
